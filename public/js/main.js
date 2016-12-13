372c305c981095a3ac1d53ec135a6f0f71f77263
Waleed Ashraf <waleedashraf@outlook.com>
2016-12-13 23:42:41 +0500

    performance fix

--- public/js/main.js
+++ public/js/main.js
@@ -189,7 +189,7 @@
 	/*----------------------------------------------------*/
 	/* Final Countdown Settings
 	------------------------------------------------------ */
-	var finalDate = '2017/01/03';
+	var finalDate = '2017/01/01';
 
 	$('div#counter').countdown(finalDate)
    	.on('update.countdown', function(event) {
