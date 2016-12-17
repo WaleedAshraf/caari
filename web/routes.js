bd742192bf94362c30bdabbc766e5099896c855b
Waleed Ashraf <waleedashraf@outlook.com>
2016-12-18 02:07:06 +0500

    pageNotfound comp removed

--- web/routes.js
+++ web/routes.js
@@ -5,7 +5,6 @@ import { Route, IndexRoute } from 'react-router'
 import Layout from './components/Layout';
 import IndexPage from './components/IndexPage';
 import WishPage from './components/WishPage';
-import NotFoundPage from './components/NotFoundPage';
 
 const routes = (
   <Route path="/" component={Layout}>
