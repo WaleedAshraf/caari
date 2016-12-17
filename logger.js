8e7c0a63208f73e5c91b8b1eefd950180078f24e
Waleed Ashraf <waleedashraf@outlook.com>
2016-12-18 01:37:45 +0500

    logger config

---
+++ logger.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var winston = require('winston');
+winston.emitErrs = true;
+
+var logger = new(winston.Logger)({
+  levels: {
+    emerg: 7,
+    alert: 6,
+    crit: 5,
+    error: 4,
+    warning: 3,
+    notice: 2,
+    info: 1,
+    debug: 0,
+  },
+  colors: {
+    emerg: 'red',
+    alert: 'yellow',
+    crit: 'red',
+    error: 'red',
+    warning: 'red',
+    notice: 'yellow',
+    info: 'green',
+    debug: 'blue',
+  },
+  transports: [
+    new (winston.transports.Console)({
+      colorize: true,
+      handleExceptions: true,
+      json: false,
+      level: "emerg"
+    }),
+    new (winston.transports.File)({
+      filename: 'trace.log',
+      colorize: true,
+      handleExceptions: true,
+      json: false,
+      level: "emerg"
+    })
+  ]
+});
+
+module.exports = logger;
