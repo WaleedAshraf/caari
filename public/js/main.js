727b0d5760648273176abb5187f6cf5494229732
Waleed Ashraf <waleedashraf@outlook.com>
2017-11-12 20:48:20 +0500

    getTime fixed

--- public/js/main.js
+++ public/js/main.js
@@ -191,7 +191,7 @@
 	------------------------------------------------------ */
 
 	function getOneMonth() {
-		return new Date.gettime() + (30 * 24 * 60 * 60 * 1000);
+		return new Date().getTime() + (30 * 24 * 60 * 60 * 1000);
 	}
 
 	var clock = $('div#counter');
